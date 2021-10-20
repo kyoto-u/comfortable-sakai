31bb9632efa06efed8296a5f617b8d5523538d70
das08 <takeda5955@gmail.com>
2021-10-20 13:16:23 +0900

    fix: 修正

--- src/tests/network.test.ts
+++ src/tests/network.test.ts
@@ -1,7 +1,6 @@
 import { getAssignmentByCourseID } from "../network";
 import fs from "fs";
-import {Assignment, AssignmentEntry, CourseSiteInfo} from "../model";
-
+import { Assignment, AssignmentEntry, CourseSiteInfo } from "../model";
 
 describe("testapi()", (): void => {
   beforeEach(() => {
@@ -9,18 +8,16 @@ describe("testapi()", (): void => {
     fetch.resetMocks();
   });
 
-  test("api", async (): Promise<void> => {
-    const jsonObject = JSON.parse(fs.readFileSync(`./src/tests/resources/assignment1.json`, 'utf8'));
+  test("single assignment", async (): Promise<void> => {
+    const jsonObject = JSON.parse(fs.readFileSync("./src/tests/resources/assignment1.json", "utf8"));
     //@ts-ignore
     fetch.mockResponseOnce(JSON.stringify(jsonObject));
     const a = await getAssignmentByCourseID("", "");
     const assignmentEntry = new AssignmentEntry("sample1","Sample Assignment1",1634731200,false, false,false);
     assignmentEntry.assignmentDetail = "--------";
     assignmentEntry.assignmentPage = "/portal/site/";
-    const assignment = new Assignment(new CourseSiteInfo("", ""), [assignmentEntry],false)
+    const assignment = new Assignment(new CourseSiteInfo("", ""), [assignmentEntry], false);
 
     expect(a).toEqual(assignment);
-
-    // expect(await getAssignmentByCourseID("", "")).toBe("");
   });
 });
