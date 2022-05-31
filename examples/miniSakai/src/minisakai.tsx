798903b7d9b00ea38e8ec06c5eae94e3c71c4b69
das08 <41512077+das08@users.noreply.github.com>
2022-06-01 00:37:06 +0900

    fix

--- examples/miniSakai/src/minisakai.tsx
+++ examples/miniSakai/src/minisakai.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { EntryUnion, MiniSakaiEntryList } from "../../../src/components/entryTab";
+import { EntryUnion, MiniSakaiEntryList } from "comfortable-sakai-component/src/components/entryTab";
 import { Settings } from "comfortable-sakai-component/src/features/setting/types";
-import { Course } from "../../../src/features/course/types";
-import { AssignmentEntry } from "../../../src/features/entity/assignment/types";
+import { Course } from "comfortable-sakai-component/src/features/course/types";
+import { AssignmentEntry } from "comfortable-sakai-component/src/features/entity/assignment/types";
 import "./comfortable-sakai.css";
 
 type EntryWithCourse = {
