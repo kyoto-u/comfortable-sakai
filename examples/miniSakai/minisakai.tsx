621b356cc2b99a263eb1e3dce8b373bd9edb6fa8
das08 <41512077+das08@users.noreply.github.com>
2022-05-31 23:43:59 +0900

    add example

---
+++ examples/miniSakai/minisakai.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { EntryUnion, MiniSakaiEntryList } from "../../src/components/entryTab";
+import { Settings } from "../../src/features/setting/types";
+import { Course } from "../../src/features/course/types";
+import { AssignmentEntry } from "../../src/features/entity/assignment/types";
+
+type EntryWithCourse = {
+    entry: EntryUnion;
+    course: Course;
+};
+import "../comfortable-sakai.css";
+
+export const Example = () => {
+    const settings = new Settings();
+    const elements: Array<EntryWithCourse> = [
+        {
+            course: new Course("12345", "Sample", ""),
+            entry: new AssignmentEntry("12", "test", 1654044495, 1654044495, false)
+        }
+    ];
+    return (
+        <MiniSakaiEntryList
+            dueType="danger"
+            isSubset={false}
+            settings={settings}
+            entriesWithCourse={elements}
+            onCheck={() => {
+                return null;
+            }}
+            onDelete={() => {
+                return null;
+            }}
+        />
+    );
+}
