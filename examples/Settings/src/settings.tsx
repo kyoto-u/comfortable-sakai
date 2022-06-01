53ef8a928ac166818e400f1acee80995179d85f7
das08 <41512077+das08@users.noreply.github.com>
2022-06-01 16:47:02 +0900

    fix imports

--- examples/Settings/src/settings.tsx
+++ examples/Settings/src/settings.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
-
-// import { Settings } from "@das08/comfortable-sakai-component/src/features/setting/types";
+import { Settings } from "@das08/comfortable-sakai-component/src/features/setting/types";
+import { SettingsTab } from "@das08/comfortable-sakai-component/src/components/settings";
 
 import "./comfortable-sakai.css";
-import { SettingsTab } from "../../../src/components/settings";
-import { Settings } from "../../../src/features/setting/types";
 
 export default function App() {
     const settings = new Settings();
